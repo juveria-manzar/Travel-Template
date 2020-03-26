6019fce3d15dd226c10c854aab4ad683d4ff7c5f
Juveria Manzar <juveriadot@gmail.com>
2020-03-27 03:59:56 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function enterSite() {
+    $('#landing').css('transform', 'translate(-200vw)')
+    $('#dimmed-bg').css('visibility', 'visible')
+    $('#dimmed-bg').css('transform', 'translate(0)')
+
+    $('#above-fold').css('opacity', '1')
+    $('#full-site').css('visibility', 'visible')
+    $('#full-site').css('overflow', 'visible')
+    $('#full-site').css('max-height', 'auto')
+    $('#register-side-bar').css('transform', 'translate(0)')
+}
