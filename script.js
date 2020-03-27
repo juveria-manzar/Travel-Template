5fa1dd1f63b5e2d1c3d1bd5afe83da8fdcddd953
Juveria Manzar <juveriadot@gmail.com>
2020-03-28 02:27:41 +0530

    Updated js

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function enterSite() {
     $('#full-site').css('max-height', 'auto')
     $('#register-side-bar').css('transform', 'translate(0)')
 }
+
+
+function openForm() {
+    $('#register-side-bar').css('transform', 'translate(-650px)')
+    $('#register-side-bar').css('transition-delay', '0')
+    $('#register-side-bar').css('z-index', '100')
+
+    $('#call-num').css('opacity', '0')
+    $('#reg').css('opacity', '0')
+}
+
+function closeForm() {
+    $('#register-side-bar').css('transform', 'translate(0)')
+    $('#register-side-bar').css('z-index', '0')
+
+    $('#call-num').css('opacity', '1')
+    $('#reg').css('opacity', '1')
+
+}
